5ebc6ac997834c01c8b26ff712f7b1aecd874de6
KingPigGameStudios / KPGS <jiovani.vianey@gmail.com>
2021-09-30 19:01:22 +0200

    Update Liaison.cs

--- AppLiaison/Liaison.cs
+++ AppLiaison/Liaison.cs
@@ -8,17 +8,21 @@ namespace AppLiaison
 {
     public class Liaison
     {
-        private string depart
-        private string arrivee
-        private string heure
+        private string depart;
+        private string arrivee;
+        private string heure;
 
         /// 
 
-        /// 
+        /// Liaison
+        public Liaison(string unDepart, string unArrivee, string unHeure) {
+            this.depart = unDepart;
+            this.arrivee = unArrivee;
+            this.heure = unHeure;
+        }
 
         /// Description
-        public string Description
-		{
+        public string Description {
 			get{return "Départ: "+ depart + " , Arrivée: "+ arrivee + " Heure " + heure;}
 		}
 
