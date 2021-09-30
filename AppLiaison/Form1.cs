d85a17d2d730f8fa37829bc70753f5f137857a72
v4nkor <vanknpix@gmail.com>
2021-09-30 15:42:25 +0200

    Modifs originales formulaire
    Modif titre, ajout liste et menustrip + boutton  + classe liaison

--- AppLiaison/Form1.cs
+++ AppLiaison/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AppLiaison
 {
-    public partial class Form1 : Form
+    public partial class AppLiaison : Form
     {
-        public Form1()
+        public AppLiaison()
         {
             InitializeComponent();
         }
--- AppLiaison/Program.cs
+++ AppLiaison/Program.cs
@@ -16,7 +16,7 @@ namespace AppLiaison
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AppLiaison());
         }
     }
 }
