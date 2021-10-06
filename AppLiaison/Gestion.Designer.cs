074079e2101ec8702bd329fa82e6743a3b305f9f
v4nkor <vanknpix@gmail.com>
2021-10-06 09:28:40 +0200

    ajout image

--- AppLiaison/Gestion.Designer.cs
+++ AppLiaison/Gestion.Designer.cs
@@ -29,6 +29,7 @@ namespace AppLiaison
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gestion));
             this.lb = new System.Windows.Forms.ListBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.modificationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -36,15 +37,17 @@ namespace AppLiaison
             this.modifierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.suprimerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lb
             // 
             this.lb.FormattingEnabled = true;
-            this.lb.Location = new System.Drawing.Point(12, 27);
+            this.lb.Location = new System.Drawing.Point(39, 60);
             this.lb.Name = "lb";
-            this.lb.Size = new System.Drawing.Size(362, 277);
+            this.lb.Size = new System.Drawing.Size(388, 277);
             this.lb.TabIndex = 0;
             // 
             // menuStrip1
@@ -53,7 +56,7 @@ namespace AppLiaison
             this.modificationsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(386, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(468, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -70,28 +73,28 @@ namespace AppLiaison
             // ajouterToolStripMenuItem
             // 
             this.ajouterToolStripMenuItem.Name = "ajouterToolStripMenuItem";
-            this.ajouterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ajouterToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
             this.ajouterToolStripMenuItem.Text = "Ajouter";
             this.ajouterToolStripMenuItem.Click += new System.EventHandler(this.ajouterToolStripMenuItem_Click);
             // 
             // modifierToolStripMenuItem
             // 
             this.modifierToolStripMenuItem.Name = "modifierToolStripMenuItem";
-            this.modifierToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.modifierToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
             this.modifierToolStripMenuItem.Text = "Modifier";
             this.modifierToolStripMenuItem.Click += new System.EventHandler(this.modifierToolStripMenuItem_Click);
             // 
             // suprimerToolStripMenuItem
             // 
             this.suprimerToolStripMenuItem.Name = "suprimerToolStripMenuItem";
-            this.suprimerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.suprimerToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
             this.suprimerToolStripMenuItem.Text = "Suprimer";
             this.suprimerToolStripMenuItem.Click += new System.EventHandler(this.suprimerToolStripMenuItem_Click);
             // 
             // btn
             // 
             this.btn.Enabled = false;
-            this.btn.Location = new System.Drawing.Point(118, 324);
+            this.btn.Location = new System.Drawing.Point(146, 366);
             this.btn.Name = "btn";
             this.btn.Size = new System.Drawing.Size(159, 52);
             this.btn.TabIndex = 2;
@@ -99,19 +102,30 @@ namespace AppLiaison
             this.btn.UseVisualStyleBackColor = true;
             this.btn.Click += new System.EventHandler(this.btn_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(468, 448);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // Gestion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(386, 396);
+            this.ClientSize = new System.Drawing.Size(468, 446);
             this.Controls.Add(this.btn);
             this.Controls.Add(this.lb);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Gestion";
             this.Text = "Gestion liaisons";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,6 +140,7 @@ namespace AppLiaison
         private System.Windows.Forms.ToolStripMenuItem ajouterToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem modifierToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem suprimerToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
