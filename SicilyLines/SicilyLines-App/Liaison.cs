e4ea26a60a69013d2686d762dc8dad56c025c9bf
v4nkor <vanknpix@gmail.com>
2022-04-04 10:34:14 +0200

    Ajout SIcilyLines-App
    Duplication du projet mission 1 renomme  en SicilyLines-App pour la mission 3

---
+++ SicilyLines/SicilyLines-App/Liaison.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLiaison
+{
+    public class Liaison
+    {
+        private string depart;
+        private string arrivee;
+        private string heure;
+
+        /// 
+
+        /// Liaison
+        public Liaison(string unDepart, string unArrivee, string unHeure) {
+            this.depart = unDepart;
+            this.arrivee = unArrivee;
+            this.heure = unHeure;
+        }
+
+        /// Description
+        public string Description {
+			get{return "Départ: "+ this.depart + " , Arrivée: "+ this.arrivee + " - Heure " + this.heure;}
+		}
+
+        public string Depart { get => depart; set => depart = value; }
+        public string Arrivee { get => arrivee; set => arrivee = value; }
+        public string Heure { get => heure; set => heure = value; }
+    }
+}
